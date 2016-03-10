46886658cbf25e839ddf0e7e7a7cae03134e7cd4
Rolf Schmidt <rolf@r-schmidt-gmbh.de>
2016-03-10 16:12:24 +0100

    Be sure that a outputfilter post hook is always generated
    Hi,
    what about to always render the outputfilter post hooks by minimum 1 to be sure that the hooks exists and to have the possiblity to changes things at every hook without checking if it really got rendered?

--- Kernel/Output/Template/Document.pm
+++ Kernel/Output/Template/Document.pm
@@ -1,5 +1,8 @@
 # --
 # Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+# --
+# $origin: https://github.com/OTRS/otrs/blob/3888cdb46ce619e92c6f5330cea951c96a697b33/Kernel/Output/Template/Document.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
